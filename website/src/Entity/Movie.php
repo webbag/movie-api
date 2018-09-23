8c8217eda04ea2cd44be42270a1a8b30af1eec30
kris <kris>
2018-09-23 12:30:59 +0200

    doctrine mapping import

---
+++ website/src/Entity/Movie.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Movie
+ *
+ * @ORM\Table(name="movie")
+ * @ORM\Entity
+ */
+class Movie
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="mo_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $moId;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="mo_title", type="string", length=255, nullable=true)
+     */
+    private $moTitle;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="mo_description", type="text", length=65535, nullable=true)
+     */
+    private $moDescription;
+
+    public function getMoId(): ?int
+    {
+        return $this->moId;
+    }
+
+    public function getMoTitle(): ?string
+    {
+        return $this->moTitle;
+    }
+
+    public function setMoTitle(?string $moTitle): self
+    {
+        $this->moTitle = $moTitle;
+
+        return $this;
+    }
+
+    public function getMoDescription(): ?string
+    {
+        return $this->moDescription;
+    }
+
+    public function setMoDescription(?string $moDescription): self
+    {
+        $this->moDescription = $moDescription;
+
+        return $this;
+    }
+
+
+}
---
+++ website/src/Entity/Rating.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rating
+ *
+ * @ORM\Table(name="rating", indexes={@ORM\Index(name="rating_movie_mo_id_fk", columns={"movie_mo_id"})})
+ * @ORM\Entity
+ */
+class Rating
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ra_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $raId;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="ra_rating", type="smallint", nullable=true)
+     */
+    private $raRating;
+
+    /**
+     * @var \Movie
+     *
+     * @ORM\ManyToOne(targetEntity="Movie")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="movie_mo_id", referencedColumnName="mo_id")
+     * })
+     */
+    private $movieMo;
+
+    public function getRaId(): ?int
+    {
+        return $this->raId;
+    }
+
+    public function getRaRating(): ?int
+    {
+        return $this->raRating;
+    }
+
+    public function setRaRating(?int $raRating): self
+    {
+        $this->raRating = $raRating;
+
+        return $this;
+    }
+
+    public function getMovieMo(): ?Movie
+    {
+        return $this->movieMo;
+    }
+
+    public function setMovieMo(?Movie $movieMo): self
+    {
+        $this->movieMo = $movieMo;
+
+        return $this;
+    }
+
+
+}
