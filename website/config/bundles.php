34f2821e16d8871de24eabbf39412efc3f31244a
kris <kris>
2018-09-23 11:27:35 +0200

    install: maker, jms serializer, fos rest

--- website/config/bundles.php
+++ website/config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
