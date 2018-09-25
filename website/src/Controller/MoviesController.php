b492ce28b24c61724a55b6a9e1828b236efed46b
kris <kris>
2018-09-25 20:07:21 +0200

    rating

--- website/src/Controller/MoviesController.php
+++ website/src/Controller/MoviesController.php
@@ -5,54 +5,69 @@ namespace App\Controller;
 use App\Services\Movies;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MoviesController extends FOSRestController
 {
-
     /**
      * @var Movies
      */
     protected $movies;
 
     /**
-     * @var Request
-     */
-    protected $request;
-
-    /**
      * MoviesController constructor.
      *
      * @param Movies $movies
      */
-    public function __construct(Movies $movies, Request $request)
+    public function __construct(Movies $movies)
     {
         $this->movies = $movies;
-        $this->request = $request;
     }
 
     public function getMoviesAction()
     {
-        $dataMovies = $this->movies->getList();
+        $response = $this->movies->getList();
 
-        return $this->handleView($this->view($dataMovies, 200));
+        return $this->handleView($this->view($response, Response::HTTP_OK));
     }
 
     public function getMovieAction($movieId)
     {
-        $dataMovies = $this->movies->getElement($movieId);
+        $response = $this->movies->getElement($movieId);
 
-        return $this->handleView($this->view($dataMovies, 200));
+        return $this->handleView($this->view($response, Response::HTTP_OK));
     }
 
-    public function postMoviesRatingAction($movieId)
+    public function postMoviesRatingAction($movieId, Request $request)
     {
+        $code = Response::HTTP_CREATED;
+        try {
+            $ratingNumber = $this->validationRating($request);
+            $this->movies->createRating($movieId, $ratingNumber);
+            $response = $this->movies->getElement($movieId);
+        } catch (\Exception $e) {
+            $response = ['message' => $e->getMessage()];
+            $code = Response::HTTP_BAD_REQUEST;
+        }
 
-        $this->request->request->get('rating');
+        return $this->handleView($this->view($response, $code));
+    }
 
-        $this->movies->createRating($movieId);
-        $dataMovies = $this->movies->getElement($movieId);
+    private function validationRating($request)
+    {
+        $ratingNumber = filter_var(
+            $request->get('rating'),
+            FILTER_VALIDATE_INT,
+            ['options' => [
+                'min_range' => 1,
+                'max_range' => 10,
+            ]]);
 
-        return $this->handleView($this->view($dataMovies, 200));
+        if (!$ratingNumber) {
+            throw new \Exception('Parameters is out of range. Hint should be 1 to 10');
+        }
+
+        return $ratingNumber;
     }
 
 }
